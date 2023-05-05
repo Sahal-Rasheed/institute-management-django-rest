e19cc9271c21fa8ec845ee04983f1cf683e57cd8
Sahal-Rasheed <sahalrasheed48@gmail.com>
2023-05-05 21:22:22 +0530

    "S3 Bucket"

--- Institute Server/institute/settings.py
+++ Institute Server/institute/settings.py
@@ -175,3 +175,14 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rishalvr@gmail.com'
 EMAIL_HOST_PASSWORD = 'kxvytomucefnqvmv'
+
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'djangoinstitutebucket'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
