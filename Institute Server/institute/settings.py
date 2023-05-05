c32e39e691d3f28d73bb1655be83a118af6dbfdc
Sahal-Rasheed <sahalrasheed48@gmail.com>
2023-05-05 20:23:11 +0530

    "static files deployement"

--- Institute Server/institute/settings.py
+++ Institute Server/institute/settings.py
@@ -152,13 +152,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-MEDIA_URL = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# MEDIA_ROOT =  os.path.join(BASE_DIR,'media')
+MEDIA_ROOT =  os.path.join(BASE_DIR,'media')
 
 
 # Default primary key field type
