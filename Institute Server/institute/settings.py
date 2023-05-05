305d6ab204a64fb34d32b635c9e2a5a60b009490
Sahal-Rasheed <sahalrasheed48@gmail.com>
2023-05-05 19:17:22 +0530

    "static"

--- Institute Server/institute/settings.py
+++ Institute Server/institute/settings.py
@@ -151,9 +151,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
-MEDIA_ROOT =  os.path.join(BASE_DIR,'media')
+# MEDIA_ROOT =  os.path.join(BASE_DIR,'media')
 
 
 # Default primary key field type
--- Institute Server/institute/urls.py
+++ Institute Server/institute/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.conf.urls.static import static
+# from django.conf import settings
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import routers
@@ -63,4 +64,6 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-] + router.urls + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + router.urls 
+
+urlpatterns += staticfiles_urlpatterns()
