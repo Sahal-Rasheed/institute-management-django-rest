dcdd9840b91fa9183545b0f69dc51da41bc1de67
Sahal-Rasheed <sahalrasheed48@gmail.com>
2023-05-06 16:06:28 +0530

    "new"

--- Institute Server/institute/settings.py
+++ Institute Server/institute/settings.py
@@ -187,6 +187,7 @@ AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
  
